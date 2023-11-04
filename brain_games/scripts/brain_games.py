9af493b174c3e21bc2b7a551567d3e1beb8ea03d
ShelbaSK <ku4umov.sergey@gmail.com>
2023-11-04 19:21:16 +0700

    added question about name of user

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+from ..cli import welcome_user
 
 def greetings():
     print('Welcome to the Brain Games!')
 
 def main():
     greetings()
+    welcome_user()
 
 if __name__ == '__main__':
     main()
