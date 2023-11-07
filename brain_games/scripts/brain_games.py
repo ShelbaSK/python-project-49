44efe739071270d6b10481db3890ec9854fbe210
ShelbaSK <ku4umov.sergey@gmail.com>
2023-11-07 21:06:04 +0700

    added flake8 and fixed comments

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 from ..cli import welcome_user
 
+
 def greetings():
     print('Welcome to the Brain Games!')
+
 
 def main():
     greetings()
     welcome_user()
+
 
 if __name__ == '__main__':
     main()
